25a23920430b5f849db662f3072c59f4d06c5be2
dedoa82 <67242566+dedoa82@users.noreply.github.com>
2021-07-17 19:25:04 -0700

    User Cart End Point

---
+++ app/Http/Controllers/Api/UserController.php
@@ -0,0 +1,74 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\Http\Controllers\Controller;
+use App\User;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
+
+class UserController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, $id)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function cart(User $user)
+    {
+        $usercart = DB::table('products')
+        ->join('carts','products.id','carts.product_id')
+        ->where('carts.user_id',$user->id)
+        ->paginate(5);
+
+        return response()->json([
+            'success' => true,
+            'payload' => $usercart
+        ]);
+    }
+}
--- routes/api.php
+++ routes/api.php
@@ -138,16 +138,22 @@ Route::group([
      * User 
      */
 
+    //User CRUD
+
+    //Route::apiResource('users', 'Api\UserController');
+
     //User Cart
+
+    Route::get('user/{user}/cart', 'Api\UserController@cart');
     
 });
 
-//Product's Color
-//Product's Size
-// Cart_product view
+// return to Handling Product's Images, Product's Color, Product's Sizes 
+
 // Address Module
 // Order Module
 // Orderproducts Module
 // Favorite Module
+// User Module
 
 // return to Handling Middlewares, Securty
